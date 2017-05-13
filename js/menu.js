da3f93864dbcdf72e9ac8d1d8fd34d8b6b4c8a3a
Artyom Rudavin <arrudavin7@gmail.com>
2017-05-13 12:19:40 +0300

    added changes for menu

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,30 @@
 $(function() {
 
-	var $links = $('.menu a');
-
-	$links.on('click', function(e) {
-		var $submenu = $(this).siblings('.submenu');
-		e.preventDefault();
-		console.log($submenu);
-    // $submenu.show(); // тоже самое что и: $submenu.css('display', 'block')
-    // $submenu.toggle(); // display: none -> display: block
-    $submenu.slideToggle();
-
-
-  })
+	$('.dropdown').hover(
+		function(){
+			$(this).children('.submenu').slideDown(200);
+		},
+		function(){
+			$(this).children('.submenu').slideUp(200);
+		}
+		);
+	
 });
+
+
+// $(function() {
+
+// 	var $links = $('.menu a');
+
+// 	$links.on('click', function(e) {
+// 		var $submenu = $(this).siblings('.submenu');
+// 		e.preventDefault();
+// 		console.log($submenu);
+//     // $submenu.show(); // тоже самое что и: $submenu.css('display', 'block')
+//     // $submenu.toggle(); // display: none -> display: block
+//     $submenu.slideToggle();
+
+
+//   })
+// });
+
